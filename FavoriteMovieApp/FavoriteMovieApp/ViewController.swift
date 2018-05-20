414fe14375ef7df5351f5db9ce0b75f7adc25d4d
Unknown <derek@core10.io>
2018-05-19 22:31:17 -0400

    [Feature] First push for new project

---
+++ FavoriteMovieApp/FavoriteMovieApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FavoriteMovieApp
+//
+//  Created by Derek Clevenger on 5/19/18.
+//  Copyright © 2018 Derek Clevenger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
