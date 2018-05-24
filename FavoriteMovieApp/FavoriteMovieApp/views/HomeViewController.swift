781626f5c22db050f3838e78bab28beffb9147d0
Unknown <derek@core10.io>
2018-05-23 21:28:36 -0400

    [Feature] Title and background set

--- FavoriteMovieApp/FavoriteMovieApp/views/HomeViewController.swift
+++ FavoriteMovieApp/FavoriteMovieApp/views/HomeViewController.swift
@@ -10,18 +10,20 @@ import UIKit
 
 class HomeViewController: UIViewController {
     var backgroundImage = UIImageView()
-
+    var titleLabel = UILabel()
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.white
         setupBackgroundImage()
+        setupTitleLabel()
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         layoutBackgroundImage()
+        layoutTitleLabel()
     }
     
 
@@ -35,9 +37,28 @@ class HomeViewController: UIViewController {
     
     func layoutBackgroundImage() {
         NSLayoutConstraint.activate([
-            backgroundImage.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.height * 0.75),
+            backgroundImage.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.height * 0.8),
             backgroundImage.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width),
             backgroundImage.topAnchor.constraint(equalTo: view.topAnchor, constant: 0.0),
+            
+            ])
+    }
+    
+    func setupTitleLabel() {
+        titleLabel.text = "Favorite Movies App"
+        titleLabel.font = UIFont(name: "ArialMT", size: 55)
+        titleLabel.numberOfLines = 3
+        titleLabel.textAlignment = .center
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        backgroundImage.addSubview(titleLabel)
+    }
+    
+    func layoutTitleLabel() {
+        NSLayoutConstraint.activate([
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: UIScreen.main.bounds.width * 0.125),
+            titleLabel.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width * 0.75),
+            titleLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: UIScreen.main.bounds.height * 0.15),
             
             ])
     }
